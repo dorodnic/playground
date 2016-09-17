3dfb64ba04ffa3d28558dc1b90518509d466402f
dorodnic <dorodnic@gmail.com>
2016-09-17 16:17:04 +0100

    some small adjustments

--- src/bind.h
+++ src/bind.h
@@ -5,6 +5,8 @@
 #include <memory>
 #include <string>
 
+#include "parser.h"
+
 class IProperty;
 
 typedef std::function<void(IProperty* prop, 
@@ -35,6 +37,8 @@ public:
 class IDataContext : public IVirtualBase
 {
 public:
+    virtual const std::string& get_type() const = 0;
+
     virtual const IProperty& get_property(const std::string& name) const = 0;
     virtual IProperty& get_property(const std::string& name) = 0;
     virtual const std::vector<std::string> get_property_names() const = 0;
@@ -67,15 +71,59 @@ private:
 };
 
 #define DefineClass(T) using __Type = T;\
-    return std::make_shared<DataContextBuilder<__Type>>(this) 
+    return std::make_shared<DataContextBuilder<__Type>>(this, #T) 
     
 #define AddField(F) add(&__Type::F, #F)
+
+template<class T, class S>
+class FieldProperty : public IProperty
+{
+public:
+
+    FieldProperty(IBindableObject* owner) 
+        : _owner(owner), _on_change([](IProperty*, 
+                                       const std::string&,
+                                       const std::string&){})
+    {}
+    
+    void set_value(std::string value) override
+    {
+    
+    }
+    std::string get_value() const override
+    {
+    
+    }
+    const std::string& get_type() const override
+    {
+    
+    }
+    const std::string& get_name() const override
+    {
+    
+    }
+    
+    void set_on_change(OnPropertyChangeCallback on_change) override
+    {
+        _on_change = on_change;
+    }
+    
+private:
+    OnPropertyChangeCallback _on_change;
+    IBindableObject* _owner;
+};
 
 template<class T>
 class DataContextBuilder : public IDataContext
 {
 public:
-    DataContextBuilder(IBindableObject* owner) : _owner(owner) {}
+    DataContextBuilder(IBindableObject* owner, std::string name) 
+        : _owner(owner), _name(name) {}
+        
+    const std::string& get_type() const override
+    {
+        return _name;
+    }
     
     const IProperty& get_property(const std::string& name) const 
     {
@@ -104,5 +152,6 @@ private:
     IBindableObject* _owner;
     std::vector<std::string> _property_names;
     std::map<std::string, std::shared_ptr<IProperty>> _properties;
+    std::string _name;
 };
 
